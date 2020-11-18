95b38b5426a38617a7f0a85f1e31d27cc5db8d10
Vesa Vlad <vesa.vlad07@gmail.com>
2020-11-18 21:34:14 +0200

    new modul1

---
+++ modul1/modul1.py
@@ -0,0 +1,24 @@
+print('Hello world')
+print('Hello world', 'Hello world', sep='- -')
+print('Hello world', end='$\n')
+
+hello = 'Hello world'
+
+print(hello)
+
+print('*Vesa Vlad*')
+
+number1 = 1
+number2 = 2
+number3 = number1 + number2
+print(number3)
+
+print(number1 - number2)
+
+number1 = 1
+number2 = '2'
+#number3 = number2 / number1
+#print(number3)
+
+print(dir(number1))
+print(number1.__add__(2))
